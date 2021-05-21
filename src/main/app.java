c80b7d08bf672711f347af67df15516897d7bfab
Mehmet İzmirlioğlu <mehmet@izmirlioglu.com>
2021-05-21 03:51:43 +0300

    Logo

--- src/main/app.java
+++ src/main/app.java
@@ -3,13 +3,15 @@ package main;
 import classes.Storage;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import screens.Main;
 
-import java.sql.Time;
+import java.io.InputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -28,9 +30,25 @@ public class app extends Application
         stage.setTitle("DietSystem");
         stage.setResizable(false);
         stage.setScene(new Scene(root, 400, 400));
+        root.setStyle("-fx-background-color: #eb2728");
 
-        Label lbl = new Label("DietSystem");
-        root.getChildren().add(lbl);
+        try
+        {
+            InputStream imageURL = getClass().getResourceAsStream("/img/dieticianapp-logo-white.png");
+            System.out.println(imageURL);
+            Image image = new Image(imageURL);
+            ImageView imageView = new ImageView(image);
+            imageView.setPreserveRatio(true);
+            imageView.setFitHeight(250);
+            imageView.setFitWidth(250);
+            StackPane.setAlignment(imageView, Pos.CENTER);
+            root.getChildren().add(imageView);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Logo can not loaded!");
+            e.printStackTrace();
+        }
 
         stage.show();
 
